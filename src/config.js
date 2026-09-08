8787b5a7ee8dc11fec4b2c77d09e7c973fd7334d
betancourtjair <jair@fpt.com.mx>
2026-09-08 11:07:28 -0600

    Actualiza src/config.js

--- src/config.js
+++ src/config.js
@@ -38,6 +38,8 @@ const config = {
   databaseUrl: required('DATABASE_URL'),
   pepper: Buffer.from(required('SECRET_PEPPER'), 'base64'),
   maxSecretBytes: int('MAX_SECRET_BYTES', 100000),
+  maxFiles: int('MAX_FILES', 5),
+  maxFilesBytes: int('MAX_FILES_BYTES', 5 * 1024 * 1024),
   maxTtlHours: int('MAX_TTL_HOURS', 168),
   cleanupIntervalMinutes: int('CLEANUP_INTERVAL_MINUTES', 5),
   graph,
